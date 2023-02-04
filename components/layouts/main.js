5ff0f6566458285c9c15419a977b8aa663766b3b
Henoker <hennybany@gmail.com>
2023-02-04 16:11:51 +0300

    feat: navbar

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,13 +1,16 @@
 import Head from "next/head";
 import {Box,Container} from '@chakra-ui/react';
+import Navbar from "../navbar";
 
 const Main = ({ children, router}) => {
     return (
         <Box as="main" pb={8}>
             <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            
                 <title>Henock Tesfatsion - HomePage</title>
-                <Container maxW="container-md"pt={14}>
+                <Navbar path={router.asPath}/>
+                <Container maxW="container.md" pt={14}>
                     {children}
                 </Container>
             </Head>
