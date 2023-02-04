31fa87eda43efc72d65f1110844deab48aa50b84
Henoker <hennybany@gmail.com>
2023-02-04 12:23:32 +0300

    initial:  project configurations

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+    arrowParens:'avoid',
+    singleQuote: true,
+    bracketSpacing: true,
+    endOfLine: 'if',
+    semi: false,
+    tabwidth: 2, 
+    trailingComma: 'none' 
+}
+
+module.exports = options
