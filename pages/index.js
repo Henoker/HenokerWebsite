e4ec29b44d08c9485025236f8d092c0f90af4d38
Henoker <hennybany@gmail.com>
2023-02-06 16:10:54 +0300

    feat: profile Image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import { Box, Container, Heading, useColorModeValue } from "@chakra-ui/react";
+import { Box, Container, Heading, useColorModeValue, Image } from "@chakra-ui/react";
+
 
 const Page= ()=>{
     return(
         <Container>
-            <Box borderRadius="lg" bg="red"
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
              mb="6" p="3" mt="18" textAlign="center">
             Hello, I&apos;m a full-stack developer based in Nairobi, Kenya!
             </Box>
@@ -13,6 +14,17 @@ const Page= ()=>{
                         Henock Tesfatsion
                     </Heading>
                     <div>Full Stack Web Developer</div>
+                </Box>
+                <Box flexShrink={0} mt={{base: 4, md: 0}} ml={{md: 6}} align="center">
+                    <Image 
+                    borderColor="whiteAlpha.800" 
+                    borderWidth={2} borderStyle="solid" 
+                    maxWidth="100" 
+                    display="inline-block" 
+                    borderRadius="full" 
+                    src="/images/suitphoto.PNG"
+                    alt="Profile Image"
+                    />
                 </Box>
             </Box>
            
