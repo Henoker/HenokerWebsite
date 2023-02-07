3a610f2940fb73f49be2cc1f9db03d8a9a523989
Henoker <hennybany@gmail.com>
2023-02-07 12:07:49 +0300

    feat: work details page

---
+++ pages/works/auction.js
@@ -0,0 +1,39 @@
+import { Container, Link, Badge, List, ListItem } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Title, WorkImage, Meta } from "../../components/work";
+import p from "../../components/paragraph";
+import Layout from "../../components/layouts/article";
+
+const Work = () => {
+    return (
+        <Layout title="auction">
+            <Container>
+                <Title>
+                    E-Commerce Auction APP <Badge>2022</Badge>
+                </Title>
+                <p>
+                As part of my harvard CS50 course, I was expected to to Design an eBay-like e-commerce auction site that will allow registered users to post auction listings, place bids on listings, comment on those listings, and add listings to a watchlist. Users are able to visit a page that displays a list of all listing based on categories.
+                </p>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Github</Meta>
+                        <Link href="https://github.com/Henoker/Project_2">
+                        https://github.com/Henoker/Project_2 <ExternalLinkIcon mx="2px"/>
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Platform</Meta>
+                        <span>Windows/Mac/Linux</span>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Tech Stacks</Meta>
+                        <span>'Django', 'JavaScript', 'SQLite'</span>
+                    </ListItem>
+                </List>
+                <WorkImage src="/images/project2.png" alt="auction"/>
+            </Container>
+        </Layout>
+    )
+}
+
+export default Work
---
+++ pages/works/dashboard.js
@@ -0,0 +1,39 @@
+import { Container, Link, Badge, List, ListItem } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Title, WorkImage, Meta } from "../../components/work";
+import p from "../../components/paragraph";
+import Layout from "../../components/layouts/article";
+
+const Work = () => {
+    return (
+        <Layout title="dashboard">
+            <Container>
+                <Title>
+                    React DashBoard APP <Badge>2022</Badge>
+                </Title>
+                <p>
+                This is a react dashboard app modified from zainkeepscode tutorial using SCAMPER methodology. I have used React, Material UI, framer motions and react-apex chart for this project.
+                </p>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>website</Meta>
+                        <Link href="https://henoker.github.io/dashboard/">
+                        https://henoker.github.io/dashboard/ <ExternalLinkIcon mx="2px"/>
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Platform</Meta>
+                        <span>Windows/Mac/Linux/Android/IOS</span>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Tech Stacks</Meta>
+                        <span>'React', 'Material UI', 'Chart js'</span>
+                    </ListItem>
+                </List>
+                <WorkImage src="/images/ReactDashboard.png" alt="dashboard"/>
+            </Container>
+        </Layout>
+    )
+}
+
+export default Work
